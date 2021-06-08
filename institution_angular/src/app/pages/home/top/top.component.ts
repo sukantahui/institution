5f6ce70f6fa81fc537066d0ef9f68f177bed453a
sukantahui <huisukanta77@gmail.com>
2021-06-08 22:32:58 +0530

    added css for why us

--- institution_angular/src/app/pages/home/top/top.component.ts
+++ institution_angular/src/app/pages/home/top/top.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
+import {TopAnimation} from './top.animation';
 
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
-  styleUrls: ['./top.component.scss']
+  styleUrls: ['./top.component.scss'],
+  animations: [TopAnimation]
 })
 export class TopComponent implements OnInit {
-
+  arc = 'false';
   constructor() {
 
   }
 
   ngOnInit(): void {
 
+  }
+  toggleBounce(){
+    this.arc = this.arc === 'false' ? 'true' : 'false';
   }
 
 }
