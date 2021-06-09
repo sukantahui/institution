7c724e2ace4125457f91704af9499c733bf366a9
sukantahui <huisukanta77@gmail.com>
2021-06-09 17:20:29 +0530

    interceptor added

--- institution_angular/src/app/app.module.ts
+++ institution_angular/src/app/app.module.ts
@@ -17,6 +17,8 @@ import {MatIconModule} from '@angular/material/icon';
 
 import {NgxPrinterModule} from 'ngx-printer';
 import {HomeModule} from './pages/home/home.module';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthInterceptor} from './services/auth.interceptor';
 // import {NgxPrintModule} from "ngx-print";
 
 
@@ -42,7 +44,8 @@ import {HomeModule} from './pages/home/home.module';
     MatIconModule,
     HomeModule,
   ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy},
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+              {provide: LocationStrategy, useClass: HashLocationStrategy},
               {provide: DateAdapter, useClass: DateFormat}, {provide: DEFAULT_CURRENCY_CODE, useValue: 'INR'} ],
 
   bootstrap: [AppComponent]
