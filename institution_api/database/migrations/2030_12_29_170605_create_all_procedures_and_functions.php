bc9c9ae618d28b7c00597abd60dd88b00ee4e9ed
sukantahui <huisukanta77@gmail.com>
2021-06-13 20:16:55 +0530

    function created to count months

--- institution_api/database/migrations/2030_12_29_170605_create_all_procedures_and_functions.php
+++ institution_api/database/migrations/2030_12_29_170605_create_all_procedures_and_functions.php
@@ -19,7 +19,19 @@ class CreateAllProceduresAndFunctions extends Migration
                             SELECT * FROM users;
                         END'
         );
-        
+        DB::unprepared('drop FUNCTION IF EXISTS notional_monthly_fees_chargeable_count;
+                        CREATE FUNCTION notional_monthly_fees_chargeable_count (temp_scr_id bigint) RETURNS int
+                        DETERMINISTIC
+                        BEGIN
+                          DECLARE month_count int;
+                            select TIMESTAMPDIFF(MONTH, effective_date, CURDATE()) INTO month_count from transaction_masters
+                          inner join student_course_registrations ON student_course_registrations.id = transaction_masters.student_course_registration_id
+                          inner join transaction_details on transaction_masters.id=transaction_details.transaction_master_id
+                          where transaction_masters.student_course_registration_id=temp_scr_id and transaction_details.ledger_id= 8;
+                            RETURN month_count;
+                        END'
+        );
+
     }
 
     public function down()
