89e11f7bf2d52d6098fa5cfb6c918a67e19e7e81
sukantahui <huisukanta77@gmail.com>
2021-06-16 09:10:43 +0530

    file saver added

--- institution_angular/src/app/pages/home/top/top.component.ts
+++ institution_angular/src/app/pages/home/top/top.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 import {TopAnimation} from './top.animation';
+import {of} from 'rxjs';
+import {saveAs} from 'file-saver';
+import FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-top',
@@ -10,6 +13,12 @@ import {TopAnimation} from './top.animation';
 })
 export class TopComponent implements OnInit {
   arc = 'false';
+  private setting = {
+    element: {
+      dynamicDownload: null as HTMLElement
+    }
+  }
+
   constructor() {
 
   }
@@ -20,5 +29,48 @@ export class TopComponent implements OnInit {
   toggleBounce(){
     this.arc = this.arc === 'false' ? 'true' : 'false';
   }
+  fakeValidateUserData() {
+    return of({
+      userDate1: 1,
+      userData2: 2
+    });
+  }
+  private dyanmicDownloadByHtmlTag(arg: {
+    fileName: string,
+    text: string
+  }) {
+    if (!this.setting.element.dynamicDownload) {
+      this.setting.element.dynamicDownload = document.createElement('a');
+    }
+    const element = this.setting.element.dynamicDownload;
+    const fileType = arg.fileName.indexOf('.json') > -1 ? 'text/json' : 'text/plain';
+    element.setAttribute('href', `data:${fileType};charset=utf-8,${encodeURIComponent(arg.text)}`);
+    element.setAttribute('download', arg.fileName);
 
+    const event = new MouseEvent('click');
+    element.dispatchEvent(event);
+  }
+  dynamicDownloadTxt() {
+    this.fakeValidateUserData().subscribe((res) => {
+      this.dyanmicDownloadByHtmlTag({
+        fileName: 'My Report',
+        text: JSON.stringify(res)
+      });
+    });
+
+  }
+
+  dynamicDownloadJson() {
+    this.fakeValidateUserData().subscribe((res) => {
+      this.dyanmicDownloadByHtmlTag({
+        fileName: 'My Report.json',
+        text: JSON.stringify(res)
+      });
+    });
+  }
+
+  saveFile() {
+    const blob = new Blob(['Hello, world!'], {type: 'text/plain;charset=utf-8'});
+    FileSaver.saveAs(blob, 'hello world.txt');
+  }
 }
